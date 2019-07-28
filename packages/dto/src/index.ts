089bf80d98f55abfc40d6389f5b743a68da7cd15
igornester <inesterenko@nucleushealth.io>
2019-07-28 21:51:41 +0300

    - add missing deps to use pkg with js

--- packages/dto/src/index.ts
+++ packages/dto/src/index.ts
@@ -1,3 +1,8 @@
+// TODO: try to avoid these requirements
+import 'reflect-metadata';
+import '@nestjs/common';
+import '@nestjs/swagger';
+
 export * from './auth';
 export * from './roles';
 export * from './users';
