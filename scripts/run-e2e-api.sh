805fde0590f5cf947446cf88840b0f51aa652339
igornester <inesterenko@nucleushealth.io>
2019-08-04 14:39:23 +0300

    - move script to run e2e

--- scripts/run-e2e-api.sh
+++ scripts/run-e2e-api.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+export DIR=$(cd `dirname $0` && pwd)
+export SERVER_PATH="${DIR}/../packages/server"
+export CLIENT_API_PATH="${DIR}/../packages/client-api"
+
 function startServerInBackground() {
+    cd "${SERVER_PATH}" || exit
     echo "Start server in background"
     ts-node -r tsconfig-paths/register src/main.ts &
     SERVER_PID=$!
@@ -9,15 +14,21 @@ function startServerInBackground() {
     echo "Wait for ${TIMEOUT_SEC} sec, to make sure server is up and running"
     sleep ${TIMEOUT_SEC}
     echo "$SERVER_PID"
+    cd "${DIR}" || exit
+}
+
+function loadInitialServerData() {
+    cd "${SERVER_PATH}" || exit
+    echo "Start loading init data"
+    ts-node script/load.intit.data.ts
+    cd "${DIR}" || exit
 }
 
 export NODE_ENV="development"
 export API_AUTH_ENABLED="false"
 
 startServerInBackground
-
-echo "Start loading init data"
-ts-node script/load.intit.data.ts
+loadInitialServerData
 
 echo "Stop server"
 kill ${SERVER_PID}
@@ -27,8 +38,9 @@ export API_AUTH_ENABLED="true"
 startServerInBackground
 
 echo "Start e2e tests"
-#jest --config ./test/jest-e2e.
-npm run test:e2e
+cd "${CLIENT_API_PATH}" || exit
+npm run test
+cd "${DIR}" || exit
 
 echo "Stop server"
 kill ${SERVER_PID}
