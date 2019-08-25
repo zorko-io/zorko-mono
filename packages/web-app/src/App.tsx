72f150895c35e4032ec56c2d4fd34e7b09b1a237
igornester <inesterenko@nucleushealth.io>
2019-08-25 20:12:08 +0300

    - add simple login form

--- packages/web-app/src/App.tsx
+++ packages/web-app/src/App.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Api from '@zorko/client-api';
+import { LoginForm } from './LoginForm';
 
 Api.setConfig({
   baseURL: process.env.REACT_APP_SEVER_API_URL
@@ -9,40 +9,11 @@ Api.setConfig({
 
 const App: React.FC = () => {
 
-  useEffect( () => {
-     async function fetchData() {
-       try{
-         await Api.loginAs({
-           email: 'admin@email.com',
-           password: 'qwerty'
-         });
-
-         const users = await Api.User.findMany();
-         console.log('USERS: ', users);
-       } catch (e) {
-         console.error(e);
-       }
-     }
-
-     fetchData();
-  }, []);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoginForm onSuccess={(token)=>{
+         console.log('TOKEN', { token });
+      }}/>
     </div>
   );
 };
---
+++ packages/web-app/src/LoginForm.tsx
@@ -0,0 +1,55 @@
+import { TokenDto } from '@zorko/dto';
+import {
+  Field,
+  Form,
+  Formik,
+  FormikActions,
+  FormikBag
+} from 'formik';
+import React from 'react';
+import Api from '@zorko/client-api';
+
+export interface FormValues {
+  login: string,
+  password: string,
+}
+
+export interface PropTypes {
+  readonly onSuccess: (token: TokenDto) => void;
+}
+
+export function LoginForm (props: PropTypes){
+
+  function handleSubmit(values: FormValues, actions: FormikActions<FormValues>) {
+    Api.createToken({
+      email: values.login,
+      password: values.password
+    }).then((payload) => {
+      console.log('LOGIN', {payload});
+      actions.setSubmitting(true);
+      props.onSuccess(payload);
+    }).catch((error) => {
+      console.log('ERROR', {error});
+      actions.setError(error);
+    })
+  }
+
+  return (
+    <Formik initialValues={{
+    login: '',
+    password: ''
+  }}
+  onSubmit={handleSubmit}>
+    {(form: FormikBag<PropTypes, FormValues>) => (
+      <Form autoComplete='off'>
+        <Field name="login" type="text" />
+        <Field name="password" type="text" />
+        <button type="submit">Submit</button>
+        <button onClick={()=> {
+          form.resetForm()
+        }}>Cancel</button>
+      </Form>
+    )}
+  </Formik>
+  )
+}
